6c2a51f2c6c823c5cbdbb2ce7b25a71dc8683a74
Taehyun Lim <admin@limtaehyun.dev>
2025-03-06 09:39:07 +0900

    ✨ 상품 타입 추가

--- src/types/product.ts
+++ src/types/product.ts
@@ -16,11 +16,21 @@ export interface ProductNode {
 }
 
 export enum ProductType {
+  PERSONAL_CHANNEL = "PERSONAL_CHANNEL",
   ROLE = "ROLE",
+  CULTURAL_VOUCHER = "CULTURAL_VOUCHER",
+  GIFTICONS = "GIFTICONS",
+  EXPERIENCE_BOOST = "EXPERIENCE_BOOST",
+  GOODS = "GOODS",
   OTHER = "OTHER",
 }
 
 export const ProductTypeLabel: Record<ProductType, string> = {
+  [ProductType.PERSONAL_CHANNEL]: "개인 채널",
   [ProductType.ROLE]: "역할",
+  [ProductType.CULTURAL_VOUCHER]: "문화 상품권",
+  [ProductType.GIFTICONS]: "기프티콘",
+  [ProductType.EXPERIENCE_BOOST]: "경험치 부스트",
+  [ProductType.GOODS]: "굿즈",
   [ProductType.OTHER]: "기타",
 };
